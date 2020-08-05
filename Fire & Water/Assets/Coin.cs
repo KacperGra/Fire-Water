8280c2042160cf9876290b9122a764d15ebc9209
KacperGra <kacperktbm@gmail.com>
2020-08-05 14:09:11 +0200

    Added coin animation and coin counter

--- Fire & Water/Assets/Coin.cs
+++ Fire & Water/Assets/Coin.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public GameObject pickParticles;
+    public Animator coinAnimator;
+
     private void Start()
     {
         transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        StartCoroutine(StartAnimation());
     }
+
+    IEnumerator StartAnimation()
+    {
+        yield return new WaitForSeconds(.834f);
+        coinAnimator.SetTrigger("IntroEnd");
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Player player = collision.GetComponent<Player>();
         if(player != null)
         {
+            FindObjectOfType<GameMaster>().PickCoin();
+
+            // Destroy ->
             Instantiate(pickParticles, transform.position, transform.rotation);
             Destroy(gameObject);
         }
--- Fire & Water/Assets/Scripts/Game/GameMaster.cs
+++ Fire & Water/Assets/Scripts/Game/GameMaster.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameMaster : MonoBehaviour
 {
     public bool testMode;
+
+    public Text coinNumberText;
+    [HideInInspector] public int coinNumber;
+
     private void Start()
     {
+        coinNumber = 0;
         IgnoreCollisionsInit();
+    }
+
+    private void Update()
+    {
+        coinNumberText.text = coinNumber.ToString();
     }
 
     private void IgnoreCollisionsInit()
@@ -24,6 +35,11 @@ public class GameMaster : MonoBehaviour
         // Monster
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Monster"), LayerMask.NameToLayer("Monster"));
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Decoration"), LayerMask.NameToLayer("Monster"));
+    }
+
+    public void PickCoin()
+    {
+        ++coinNumber;
     }
 
     public void GameOver()
