3e3929440ae47a973c1db94844b5c3773f43410d
KacperGra <kacperktbm@gmail.com>
2020-08-07 16:31:04 +0200

    Shop added

---
+++ Fire & Water/Assets/Scripts/Game/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Create Item")]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public int cost;
+    public Sprite image;
+}
---
+++ Fire & Water/Assets/Scripts/Game/Shop_Skill.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop_Skill : MonoBehaviour
+{
+    public Item item;
+
+    public Text nameText;
+    public Image image;
+    public Text costText;
+
+    private void Start()
+    {
+        nameText.text = item.itemName;
+        image.sprite = item.image;
+        var buyText = "Buy: ";
+        costText.text = buyText + item.cost.ToString();
+    }
+}
